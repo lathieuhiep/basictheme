3ecb611228e9afd78f5aac57103bfd7098fb241c
lakhacdiep <diep.lk@beetechsoft.vn>
2022-03-09 14:23:06 +0700

    update core theme

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,8 @@ let gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     minifyCss = require('gulp-clean-css'),
-    concatCss = require('gulp-concat-css'),
-    rename = require('gulp-rename');
-
+    concatCss = require('gulp-concat-css');
+// rename = require('gulp-rename');
 // sass.compiler = require('node-sass');
 
 // Task sass style theme
@@ -22,19 +21,33 @@ gulp.task('sass-style-theme', function () {
         .pipe(gulp.dest('./'));
 });
 
-// Task sass library theme
-gulp.task('sass-library-theme', function () {
-    return gulp.src('./assets/scss/library.scss')
-        .pipe(sass({
-            outputStyle: 'expanded'
-        }).on('error', sass.logError))
+// Task compress mini library css theme
+gulp.task('compress-css', function () {
+    return gulp.src([
+        './node_modules/bootstrap/dist/css/bootstrap.css',
+        './node_modules/owl.carousel/dist/assets/owl.carousel.css'
+    ])
+        .pipe(concatCss("library.min.css"))
         .pipe(minifyCss({
             compatibility: 'ie8',
             level: {1: {specialComments: 0}}
         }))
-        .pipe(rename('library.min.css'))
         .pipe(gulp.dest('./assets/css/'));
 });
+
+// Task sass library theme
+// gulp.task('sass-library-theme', function () {
+//     return gulp.src('./assets/scss/library.scss')
+//         .pipe(sass({
+//             outputStyle: 'expanded'
+//         }).on('error', sass.logError))
+//         .pipe(minifyCss({
+//             compatibility: 'ie8',
+//             level: {1: {specialComments: 0}}
+//         }))
+//         .pipe(rename('library.min.css'))
+//         .pipe(gulp.dest('./assets/css/'));
+// });
 
 // Task compress lib js & mini file
 gulp.task('compress-js', function () {
@@ -45,15 +58,4 @@ gulp.task('compress-js', function () {
         .pipe(concat('library.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('./assets/js/'));
-});
-
-// Task compress mini css
-gulp.task('compress-css', function () {
-    return gulp.src('/css/library/*.css')
-        .pipe(concatCss("library.min.css"))
-        .pipe(minifyCss({
-            compatibility: 'ie8',
-            level: {1: {specialComments: 0}}
-        }))
-        .pipe(gulp.dest('/css/library/minify'));
 });
